83eb4981493fd5fc83a2c0d17cd9c1d0b3db7dc9
Minecraft Chicken Infinity <minecraft.chicken@outlook.com>
2021-07-27 00:30:26 +1000

    Update ScoreTest.js

--- ScoreTest.js
+++ ScoreTest.js
@@ -4,5 +4,4 @@ var ore_block_last_value_FHM = 0;
 
 ore_block_last_value_FHM = JSON.parse(localStorage.getItem("OreBlockLastSaveFHM"));
 TotalScoreVars = ore_block_last_value_FHM
-TotalScore = new component("75px", "Source Sans Pro", "cyan", 280, 100, "text");
-TotalScore.text = "Total Score " + TotalScoreVars
+document.getElementById("TotalGameScore").innerHTML = myFunction(TotalScoreVars);
