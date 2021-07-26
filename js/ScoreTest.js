64493bac9576a8b451851ac4afc4dc059d42762b
Minecraft Chicken Infinity <minecraft.chicken@outlook.com>
2021-07-27 01:27:50 +1000

    Update ScoreTest.js

--- js/ScoreTest.js
+++ js/ScoreTest.js
@@ -1,7 +1,10 @@
 var TotalScore;
 var TotalScoreVars = 0;
 var ore_block_last_value_FHM = 0;
+var elem = document.getElementById('TotalGameScore');
 
 ore_block_last_value_FHM = JSON.parse(localStorage.getItem("OreBlockLastSaveFHM"));
 TotalScoreVars = ore_block_last_value_FHM
-document.getElementById("TotalGameScore").innerHTML = ("Total Game Score: " + TotalScoreVars);
+if (typeof elem !== null && elem !== 'undefined' ) {
+  document.getElementById("TotalGameScore").innerHTML = ("Total Game Score: " + TotalScoreVars);
+}
