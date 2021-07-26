2ce71ee4812d63b7bf0d3c382a7e5a06a6b169d7
Minecraft Chicken Infinity <minecraft.chicken@outlook.com>
2021-07-27 01:05:55 +1000

    Delete ScoreTest.js

--- ScoreTest.js
+++
@@ -1,7 +0,0 @@
-var TotalScore;
-var TotalScoreVars = 0;
-var ore_block_last_value_FHM = 0;
-
-ore_block_last_value_FHM = JSON.parse(localStorage.getItem("OreBlockLastSaveFHM"));
-TotalScoreVars = ore_block_last_value_FHM
-document.getElementById("TotalGameScore").innerHTML = ("Total Game Score: " + TotalScoreVars);
