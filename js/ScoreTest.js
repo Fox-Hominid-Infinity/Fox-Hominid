da18653f0edfacc2c9d77cce6f3112e32778cf46
Minecraft Chicken Infinity <minecraft.chicken@outlook.com>
2021-07-29 13:18:14 +1000

    Update ScoreTest.js

--- js/ScoreTest.js
+++ js/ScoreTest.js
@@ -17,7 +17,9 @@ ScoreVars_FHM = ore_block_last_value_FHM
 ScoreVars_PPM = ore_block_last_value_PPM
 ScoreVars_FHEY = Owned_Buy_Got_Character_Object
 
-var element = document.getElementById("TotalGameScore").innerHTML = ("Total Game Score: " + TotalScoreVars);
-var element1 = document.getElementById("FHEYScore").innerHTML = ("Score: " + ScoreVars_FHEY);
-var element2 = document.getElementById("PPMScore").innerHTML = ("Score: " + ScoreVars_PPM);
-var element3 = document.getElementById("FHMScore").innerHTML = ("Score: " + ScoreVars_FHM);
+document.fillStyle = "rgb( 0,255,255)";
+document.font = "40px Source Sans Pro";
+document.fillText = ("Total Game Score: " + TotalScoreVars, 400, 80);
+document.fillText = ("Score: " + ScoreVars_FHEY, 20, 400);
+document.fillText = ("Score: " + ScoreVars_PPM, 300, 400);
+document.fillText = ("Score: " + ScoreVars_FHM, 580, 400);
