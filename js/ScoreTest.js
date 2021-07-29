726419b9ae2a1de0a953a75e76701e3e7ecf151b
Minecraft Chicken Infinity <minecraft.chicken@outlook.com>
2021-07-30 05:52:27 +1000

    Update ScoreTest.js

--- js/ScoreTest.js
+++ js/ScoreTest.js
@@ -2,18 +2,18 @@ var TotalScoreVars = 0;
 var ScoreVars_FHM = 0;
 var ScoreVars_PPM = 0;
 var ScoreVars_FHEY = 0;
-var Owned_Buy_Got_Character_Object = 0;
+var PPM_ore_block_last_value = 0;
 var ore_block_last_value_FHM = 0;
-var ore_block_last_value_PPM = 0;
+var Owned_Buy_Got_Character_Object = 0;
 
-ore_block_last_value_FHM = JSON.parse(localStorage.getItem("OreBlockLastSaveFHM"));
-ore_block_last_value_PPM = JSON.parse(localStorage.getItem("OreBlockLastSave"));
 Owned_Buy_Got_Character_Object = JSON.parse(localStorage.getItem("OwnedCharacterSaveFHEY"));
+PPM_ore_block_last_value = JSON.parse(localStorage.getItem("OreBlockLastSave"));
+ore_block_last_value_FHM = JSON.parse(localStorage.getItem("OreBlockLastSaveFHM"));
 
 TotalScoreVars = ore_block_last_value_FHM
-TotalScoreVars += ore_block_last_value_PPM
+TotalScoreVars += PPM_ore_block_last_value
 TotalScoreVars += Owned_Buy_Got_Character_Object
 
 ScoreVars_FHM += ore_block_last_value_FHM
-ScoreVars_PPM += ore_block_last_value_PPM
+ScoreVars_PPM += PPM_ore_block_last_value
 ScoreVars_FHEY += Owned_Buy_Got_Character_Object
