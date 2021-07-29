876c94405ec995adf91c1e04f89d2b18d72fa3c6
Minecraft Chicken Infinity <minecraft.chicken@outlook.com>
2021-07-29 12:32:57 +1000

    Update ScoreTest.js

--- js/ScoreTest.js
+++ js/ScoreTest.js
@@ -1,1 +1,25 @@
-//Coming Soon
+var TotalScoreVars = 0;
+var ScoreVars_FHM = 0;
+var ScoreVars_PPM = 0;
+var ScoreVars_FHEY = 0;
+var ore_block_last_value_FHM = 0;
+var ore_block_last_value_PPM = 0;
+var Owned_Buy_Got_Character_Object = 0;
+
+ore_block_last_value_FHM = JSON.parse(localStorage.getItem("OreBlockLastSaveFHM"));
+ore_block_last_value_PPM = JSON.parse(localStorage.getItem("OreBlockLastSave"));
+Owned_Buy_Got_Character_Object = JSON.parse(localStorage.getItem("OwnedCharacterSaveFHEY"));
+TotalScoreVars = ore_block_last_value_FHM
+TotalScoreVars += ore_block_last_value_PPM
+TotalScoreVars += Owned_Buy_Got_Character_Object
+
+ScoreVars_FHM = ore_block_last_value_FHM
+ScoreVars_PPM = ore_block_last_value_PPM
+ScoreVars_FHEY = Owned_Buy_Got_Character_Object
+
+if (foo !== undefined) {
+  document.getElementById("TotalGameScore").innerHTML = ("Total Game Score: " + TotalScoreVars);
+  document.getElementById("FHEYScore").innerHTML = ("Score: " + ScoreVars_FHEY);
+  document.getElementById("PPMScore").innerHTML = ("Score: " + ScoreVars_PPM);
+  document.getElementById("FHMScore").innerHTML = ("Score: " + ScoreVars_FHM);
+}
