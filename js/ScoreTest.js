cce6709f2f754b3d0295aebe67d2a2b6f011d32a
Minecraft Chicken Infinity <minecraft.chicken@outlook.com>
2021-08-09 13:02:13 +1000

    Update ScoreTest.js

--- js/ScoreTest.js
+++ js/ScoreTest.js
@@ -11,23 +11,27 @@ ore_block_last_value = JSON.parse(localStorage.getItem("OreBlockLastSave"));
 ore_block_last_value_FHM = JSON.parse(localStorage.getItem("OreBlockLastSaveFHM"));
 
 
-ore_block_last_value = JSON.parse(localStorage.getItem("OreBlockLastSave"));
-
-
 TotalScoreVars = ore_block_last_value_FHM
 TotalScoreVars += ore_block_last_value
 TotalScoreVars += Owned_Buy_Got_Character_Object
-
-TotalScoreVars += ore_block_last_value
 
 
 ScoreVars_FHM += ore_block_last_value_FHM
 ScoreVars_PPM += ore_block_last_value
 ScoreVars_FHEY += Owned_Buy_Got_Character_Object
 
-ScoreVars_PPM += ore_block_last_value
+
+if (ore_block_last_value == null) {
+  ore_block_last_value = JSON.parse(localStorage.getItem("OreBlockLastSave"));
+  TotalScoreVars += ore_block_last_value
+  ScoreVars_PPM += ore_block_last_value
+}
+
+console.log("FHM " + ore_block_last_value_FHM);
+console.log("PPM " + ore_block_last_value);
+console.log("FHEY " + Owned_Buy_Got_Character_Object);
 
 
-console.log("FHM" + ore_block_last_value_FHM);
-console.log("PPM" + ore_block_last_value);
-console.log("FHEY" + Owned_Buy_Got_Character_Object);
+console.log("FHMScore " + ScoreVars_FHM);
+console.log("PPMScore " + ScoreVars_PPM);
+console.log("FHEYScore " + ScoreVars_FHEY);
