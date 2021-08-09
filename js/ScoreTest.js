75b4a80c5dce8246b18ed7c325046a88181995ed
Minecraft Chicken Infinity <minecraft.chicken@outlook.com>
2021-08-09 12:57:24 +1000

    Update ScoreTest.js

--- js/ScoreTest.js
+++ js/ScoreTest.js
@@ -10,13 +10,22 @@ Owned_Buy_Got_Character_Object = JSON.parse(localStorage.getItem("OwnedCharacter
 ore_block_last_value = JSON.parse(localStorage.getItem("OreBlockLastSave"));
 ore_block_last_value_FHM = JSON.parse(localStorage.getItem("OreBlockLastSaveFHM"));
 
+
+ore_block_last_value = JSON.parse(localStorage.getItem("OreBlockLastSave"));
+
+
 TotalScoreVars = ore_block_last_value_FHM
 TotalScoreVars += ore_block_last_value
 TotalScoreVars += Owned_Buy_Got_Character_Object
 
+TotalScoreVars += ore_block_last_value
+
+
 ScoreVars_FHM += ore_block_last_value_FHM
 ScoreVars_PPM += ore_block_last_value
 ScoreVars_FHEY += Owned_Buy_Got_Character_Object
+
+ScoreVars_PPM += ore_block_last_value
 
 
 console.log("FHM" + ore_block_last_value_FHM);
