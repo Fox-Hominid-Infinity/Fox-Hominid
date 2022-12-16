4d2cb2487cab4ba39e6aa6fae95c62715f85a768
Minecraft Chicken Infinity <minecraft.chicken@outlook.com>
2022-12-17 01:53:26 +1100

    Update ScoreTest.js

--- js/ScoreTest.js
+++ js/ScoreTest.js
@@ -3,27 +3,31 @@ var ScoreVars_FHM = 0;;
 var ScoreVars_MCM = 0;
 var ScoreVars_PPM = 0;
 var ScoreVars_FHEY = 0;
+var ScoreVars_PPPJ = 0;
 var ore_block_last_value = 0;
 var ore_block_last_value_FHM = 0;
 var ore_block_last_value_MCM = 0;
 var Owned_Buy_Got_Character_Object = 0;
+var totallevelsvar = 0;
 
 Owned_Buy_Got_Character_Object = JSON.parse(localStorage.getItem("OwnedCharacterSaveFHEY"));
 ore_block_last_value = JSON.parse(localStorage.getItem("OreBlockLastSave"));
 ore_block_last_value_FHM = JSON.parse(localStorage.getItem("OreBlockLastSaveFHM"));
 ore_block_last_value_MCM = JSON.parse(localStorage.getItem("OreBlockLastSaveMCM"));
-
+totallevelsvar = JSON.parse(localStorage.getItem("TotalLevelSavePPPJ"));
 
 TotalScoreVars = ore_block_last_value_MCM
 TotalScoreVars += ore_block_last_value_FHM
 TotalScoreVars += ore_block_last_value
 TotalScoreVars += Owned_Buy_Got_Character_Object
+TotalScoreVars += totallevelsvar
 
 
 ScoreVars_MCM += ore_block_last_value_MCM
 ScoreVars_FHM += ore_block_last_value_FHM
 ScoreVars_PPM += ore_block_last_value
 ScoreVars_FHEY += Owned_Buy_Got_Character_Object
+ScoreVars_PPPJ += totallevelsvar
 
 
 if (ore_block_last_value == null) {
@@ -36,9 +40,11 @@ console.log("MCM " + ore_block_last_value_MCM);
 console.log("FHM " + ore_block_last_value_FHM);
 console.log("PPM " + ore_block_last_value);
 console.log("FHEY " + Owned_Buy_Got_Character_Object);
+console.log("PPPJ " + totallevelsvar);
 
 
 console.log("MCMScore " + ScoreVars_MCM);
 console.log("FHMScore " + ScoreVars_FHM);
 console.log("PPMScore " + ScoreVars_PPM);
 console.log("FHEYScore " + ScoreVars_FHEY);
+console.log("PPPJScore " + ScoreVars_PPPJ);
